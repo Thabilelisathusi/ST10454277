3d0aee535c6b3069df9e0ee40193f3dcd5e496cd
RC_Student_lab <RC_Student_lab@10.0.0.132>
2024-09-26 12:51:16 +0000

    Second commit

--- src/main/java/com/mycompany/poe1/LOGIN.java
+++ src/main/java/com/mycompany/poe1/LOGIN.java
@@ -9,5 +9,24 @@ package com.mycompany.poe1;
  * @author RC_Student_lab
  */
 public class LOGIN {
+    private String username; //store username
+    private String password; //store password
+    private String firstName; //store firstname
+    private String lastName;  //Store lastname
+
     
+    public boolean checkusername(String username) {
+        return username.contains("_") && username.length()<=5;
+         
+    }
+    
+    public boolean checkpasswordcomplexity(String password){
+        if (password.length() < 8) return false;
+       
+        boolean hasuppercase = false;
+        boolean hasdigit = false;       
+        boolean hasspecialchar = false;
+        
+       
+    }
 }
--- src/main/java/com/mycompany/poe1/POE1.java
+++ src/main/java/com/mycompany/poe1/POE1.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.poe1;
-
+import java.util.Scanner;  // Import the Scanner class
 /**
  *
  * @author RC_Student_lab
@@ -11,6 +11,29 @@ package com.mycompany.poe1;
 public class POE1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+        LOGIN loginSystem = new LOGIN();
+        
+        //Registration of varibles
+        
+        System.out.println("Registration");
+       
+        System.out.println("Enter Username");
+        String username = sc.next();
+        
+        System.out.println("Password");
+        String password = sc.next();
+        
+        System.out.println("First Name");
+        String firstName = sc.next();
+        
+        System.out.println("Last Name");
+        String lastName = sc.next();
+        
+        
+        
+        
+        
+        
     }
 }
