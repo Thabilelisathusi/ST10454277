7428246e5e62b6460601aff0abc1c1d516b0607c
RC_Student_lab <RC_Student_lab@10.0.0.132>
2024-10-30 21:18:27 +0000

    POE2

--- src/main/java/com/mycompany/poe1/Task.java
+++ src/main/java/com/mycompany/poe1/Task.java
@@ -79,6 +79,7 @@ public class Task {
                "\nDuration: " + taskDuration + " hours" +
                "\nStatus: " + taskStatus + 
                "\nTask ID: " + createTaskID(taskNumber);
-    }
 
+
+}
 }
